11e495ac090dedea5c463b06b45cae1b176295b9
Dogukan0127 <dogukangs2005@gmail.com>
2020-08-07 20:24:48 +0300

    add details

--- hamburber-icon.js
+++ hamburber-icon.js
@@ -2,6 +2,7 @@ let hamburger = document.querySelector(".hamburger")
 let list = document.querySelector(".list")
 let links = document.querySelectorAll(".list li")
 
-hamburger.addEventListener("click", () =>{
+hamburger.addEventListener("click", () => {
     list.classList.toggle("open");
 });
+
